1b357a6ad4091c3425ba4af289609abfb316fc5c
Duc Duy <0374740747s@gmail.com>
2019-03-26 22:37:59 +0700

    add slelect custom style

--- src/app/routes/booking/booking.module.ts
+++ src/app/routes/booking/booking.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking.routing';
 import { BookingComponent } from './booking.component';
+import { FlightModule } from 'src/app/components/flight/flight.module';
 
 @NgModule({
   declarations: [BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
+    BookingRoutingModule,
+    FlightModule
   ]
 })
 export class BookingModule { }
