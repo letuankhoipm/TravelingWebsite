2dafab939934be8beddf33e39ea6e4e99d7338e8
CaptainKO <17521098@gm.uit.edu.vn>
2019-03-26 17:31:27 +0700

    add new search component

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routing';
 import { HomeComponent } from './home.component';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SearchModule
   ],
   declarations: [HomeComponent],
-  exports: [HomeComponent]
+  exports: [
+    HomeComponent
+  ]
 })
 export class HomeModule { }
