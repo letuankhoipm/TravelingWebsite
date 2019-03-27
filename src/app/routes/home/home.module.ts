cad6583ab1162f4795a788192e8570d35ec75ce8
13619263 <17520648@gm.uit.edu.vn>
2019-03-27 12:54:04 +0700

    commit

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routing';
 import { HomeComponent } from './home.component';
 import { SearchModule } from './search/search.module';
+import { TourCardModule } from 'src/app/components/tour-card/tour-card.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SearchModule
+    SearchModule,
+    TourCardModule
   ],
   declarations: [HomeComponent],
   exports: [
