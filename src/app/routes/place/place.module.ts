044fd9f1258d20a590348a40db8be4cad48d808b
13619263 <17520648@gm.uit.edu.vn>
2019-04-03 08:06:02 +0700

    add place & pDetail comp

--- src/app/routes/place/place.module.ts
+++ src/app/routes/place/place.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaceComponent } from './place.component';
 import { PlaceRoutingComponent } from './place.routing';
+import { PlaceDetailComponent } from './place-detail/place-detail.component';
 
 @NgModule({
-  declarations: [PlaceComponent],
+  declarations: [
+    PlaceComponent,
+    PlaceDetailComponent],
   imports: [
     CommonModule,
     PlaceRoutingComponent
+  ],
+  exports: [
+    PlaceComponent,
+    PlaceDetailComponent
   ]
 })
 export class PlaceModule { }
