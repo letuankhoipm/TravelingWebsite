8beaa5f18bf0100915dfe8335be223e6d325fd4d
Le Tuan Khoi <17520648@gm.uit.edu.vn>
2019-04-24 15:47:50 +0700

    fix animate

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -13,6 +13,13 @@ export class SearchComponent implements OnInit {
     cruises: false
   };
 
+  cloud1 = 'assets/images/animate/clouds-1.svg';
+  cloud2 = 'assets/images/animate/clouds-2.svg';
+  cloud3 = 'assets/images/animate/clouds-3.svg';
+  boat2 = 'assets/images/animate/sail-boat2.svg';
+  taxi2 = 'assets/images/animate/taxi2.svg';
+
+
   constructor() { }
 
   ngOnInit() {
