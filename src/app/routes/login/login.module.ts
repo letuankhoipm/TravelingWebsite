d761227f6cd6095986f4d87d21f078b80004f024
Le Tuan Khoi <17520648@gm.uit.edu.vn>
2019-04-24 11:54:07 +0700

    fix some css bug

--- src/app/routes/login/login.module.ts
+++ src/app/routes/login/login.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login.routing';
+import { ShareModule } from '@shared/share/share.module';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule
+    FormsModule,
+    ShareModule
   ]
 })
 export class LoginModule { }
