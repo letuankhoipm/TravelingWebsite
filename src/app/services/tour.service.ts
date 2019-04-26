e6281fc67205aba108e7ad79da82a40a150a8115
captainko <17521098@gm.uit.edu.vn>
2019-04-26 23:07:31 +0700

    remove unnecessary imports

--- src/app/services/tour.service.ts
+++ src/app/services/tour.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
 import { BaseService } from './base.service';
 
 @Injectable()
